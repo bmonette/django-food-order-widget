89b64e0d1e926eacbe2660740185d0a81b6848da
bmonette <benoitmonette80@gmail.com>
2026-05-23 14:14:40 -0400

    Registered the Restaurant model in admin

---
+++ apps/restaurants/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Restaurant
+
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "created_at", "updated_at")
+    list_filter = ("is_active",)
+    search_fields = ("name", "slug")
+    prepopulated_fields = {"slug": ("name",)}
